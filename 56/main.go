ac099c8b02d7a01c9365bb87b517629f3c6f06db
sunnywhy <sunnywhy@gmail.com>
2022-03-13 18:04:49 -0400

    56

--- 56/main.go
+++ 56/main.go
@@ -1,30 +1,26 @@
 package main
 
 import "sort"
+
 // Merge Intervals
 func merge(intervals [][]int) [][]int {
-    if len(intervals) <= 1 {
-        return intervals
-    }
-    // sort
-    sort.Slice(intervals, func(i, j int) bool {
-        return intervals[i][0] < intervals[j][0]
-    })
-    prevEnd := intervals[0][1]
-    res := [][]int{}
-    res = append(res, intervals[0])
-    
-    for i := 1; i < len(intervals); i++ {
-        if intervals[i][0] > prevEnd {
-            res[len(res)-1][1] = prevEnd
-            res = append(res, intervals[i])
-            prevEnd = intervals[i][1]
-        }else {
-            if intervals[i][1] > prevEnd {
-                prevEnd = intervals[i][1]
-            }
-        }
-    }
-    res[len(res)-1][1] = prevEnd
-    return res
+	var res [][]int
+
+	sort.Slice(intervals, func(i, j int) bool {
+		return intervals[i][0] < intervals[j][0]
+	})
+
+	cur := intervals[0]
+	for i := 1; i < len(intervals); i++ {
+		if intervals[i][0] > cur[1] {
+			res = append(res, append([]int{}, cur...))
+			cur = intervals[i]
+		} else {
+			if intervals[i][1] > cur[1] {
+				cur[1] = intervals[i][1]
+			}
+		}
+	}
+	res = append(res, cur)
+	return res
 }
